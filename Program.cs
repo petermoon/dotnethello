9f654a0c0b0fb7f41f94461ce33bdf6ecc97893c
Peter Moon <peter.moon00@gmail.com>
2016-07-01 13:55:07 -0700

    Adding * URL

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace aspnetcoreapp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5000/")
                 .Build();
 
             host.Run();
